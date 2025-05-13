81707498c5697509091848b66115d3fe2b6f560b
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-05-13 01:42:20 -0600

    make simple boilerplate text only format
    this simple markup will how I mostly want the text to look like
    when not in edit mode

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,23 @@ function App() {
 
   return (
     <div>
-      hello
+        <h1>Resume for Didier Longoria</h1>
+        <ul>
+          <li>fakeEmail@gmail.com</li> 
+          <li>999-999-9999</li>
+        </ul>
+
+        <h2>Educational Experience</h2>
+        <p>I was at EPCC studying for CS from 2022-2024</p>
+        <p>I was at HHS studying for diploma from 2018-2022</p>
+
+
+        <h2>Practical Experience</h2>
+        <p>I was at walmart working as cashier from 2022-Present</p>
+        <p>I was at Taco Bell working as cashier from 2020-2022</p>
+
+
+
     </div>
   )
 }
